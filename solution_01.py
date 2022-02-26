8b0ce93e657c3f35177f6a4199a15a582cb4f096
Dmaina5054 <dmain5054@gmail.com>
2022-02-26 22:22:07 +0300

    solved problem2

--- solution_01.py
+++ solution_01.py
@@ -6,7 +6,10 @@ def fetchresponse():
     with requests.get(url) as response:
         print(response.status_code)
         print(response.encoding)
-        print(response.text)
+        result_list = response.json()['data']
+        print([result['email'] for result in result_list])
+        
+        
         
         
 if __name__ == '__main__':
